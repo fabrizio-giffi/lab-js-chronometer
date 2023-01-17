d35fc88a2bf1a60a6b2b7467ae3766bd6f717573
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-01-17 15:04:16 +0100

    Lab done including bonus. Jasmine is pleased. Bonus sections are commented.

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,69 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
+
+    // // Uncomment for bonus section 1/6
+    // this.centiseconds = 0
+    // this.centId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval (() => {
+      this.currentTime += 1
+      if (printTimeCallback) printTimeCallback()
+    }, 1000)
+
+    // // Uncomment for bonus section 2/6
+    // this.centId = setInterval (() => {
+    //   this.centiseconds += 1
+    //   if (printTimeCallback) printTimeCallback()
+    // }, 10)
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) return 0;
+    return this.currentTime % 60
   }
 
+  // // Uncomment for bonus section 3/6
+  // getCentiseconds() {
+  //   if (this.centiseconds === 0) return 0;
+  //   return this.centiseconds % 100
+  // }
+
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return ("0" + value).slice(-2)
+
+    // SAME RESULT WITH A DIFFERENT METHOD
+    // return (value).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
+
+    // // Uncomment for bonus section 4/6
+    // clearInterval(this.centId)
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
+
+    // // Uncomment for bonus section 5/6
+    // this.centiseconds = 0
   }
 
   split() {
-    // ... your code goes here
+    // // Uncomment for bonus section 6/6
+    // const centSplit = this.computeTwoDigitNumber(this.getCentiseconds())
+    // return `${minSplit}:${secSplit}:${centSplit}`
+
+    const minSplit = this.computeTwoDigitNumber(this.getMinutes())
+    const secSplit = this.computeTwoDigitNumber(this.getSeconds())
+    return `${minSplit}:${secSplit}`
   }
 }
